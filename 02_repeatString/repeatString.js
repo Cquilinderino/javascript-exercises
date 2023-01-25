51ccab8804a161751fa670d7a2a347daad63379f
cquilinderino <cquilinderino@gmail.com>
2023-01-25 22:49:26 +1100

    My solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let hey = string;
 
+    if (num === 0) {
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    }
+    
+    for (let i = 1; i < num; i++){
+        hey += string;
+    }
+    return hey
 };
 
 // Do not edit below this line
