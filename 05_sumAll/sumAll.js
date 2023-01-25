594720a1b5830237e5d5c3c413a5da5c814a3913
cquilinderino <cquilinderino@gmail.com>
2023-01-25 22:52:47 +1100

    My solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let firstNum, secondNum, sum = 0 ;
 
+    if (b < a) {
+        firstNum = b;
+        secondNum = a; 
+    } else {
+        firstNum = a;
+        secondNum = b;
+    } 
+
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
